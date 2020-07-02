e3b658d145ad38c2836f0042f3014901d1c0b982
peaky76 <robertjamespeacock@gmail.com>
2020-07-02 14:07:21 +0100

    imdb sql created

---
+++ start_code/db/imdb.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS castings;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS stars;
+
+CREATE TABLE stars (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
+
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(255)
+);
+
+CREATE TABLE castings (
+    id SERIAL PRIMARY KEY,
+    star_id INT REFERENCES stars(id) ON DELETE CASCADE,
+    movie_id INT REFERENCES movies(id) ON DELETE CASCADE
+);
